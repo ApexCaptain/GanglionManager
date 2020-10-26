d22e3b02e5d98bd0bb7e1c336865991f0b7f1038
SangHun Lee <ayteneve93@gmail.com>
2020-10-26 18:21:20 +0900

    Test Commit

--- dev/GitPushingTask.js
+++ dev/GitPushingTask.js
@@ -3,17 +3,22 @@
 const commandExecutor = require('./CommandExecutor.js')
 
 module.exports.gitPush = async (defaultMessage = "No Commit Message", newVersionTag) => {
-    let commitMessage
-    commitMessage = await commandExecutor.readPromisifiedText(`\nGit Commit Message (Default : ${defaultMessage}) : `)
-    if(!commitMessage) commitMessage = defaultMessage
-    await commandExecutor.runPromisifiedCommand(`git add -A`, false)
-    await commandExecutor.runPromisifiedCommand(`git commit -m "${commitMessage}"`)
-    if(newVersionTag) await commandExecutor.runPromisifiedCommand(`git tag "${newVersionTag}"`)
-    // await commandExecutor.runPromisifiedCommand(`git push origin master --tags`)
-    await commandExecutor.runPromisifiedCommand('git push --tags')
-    if(process.argv[2] == '-m') {
-        console.log("Git pushing completed")
-        process.exit(0)
+    try {
+        let commitMessage
+        commitMessage = await commandExecutor.readPromisifiedText(`\nGit Commit Message (Default : ${defaultMessage}) : `)
+        if(!commitMessage) commitMessage = defaultMessage
+        await commandExecutor.runPromisifiedCommand(`git add -A`, false)
+        await commandExecutor.runPromisifiedCommand(`git commit -m "${commitMessage}"`)
+        if(newVersionTag) await commandExecutor.runPromisifiedCommand(`git tag "${newVersionTag}"`)
+        // await commandExecutor.runPromisifiedCommand(`git push origin master --tags`)
+        await commandExecutor.runPromisifiedCommand('git push --tags')
+        if(process.argv[2] == '-m') {
+            console.log("Git pushing completed")
+            process.exit(0)
+        }
+    } catch(error) {
+        console.error(error)
+        process.exit(1)
     }
 }
 if(process.argv[2] == '-m') module.exports.gitPush()
