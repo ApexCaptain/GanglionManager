067003fde7195e238d940153eb694b018ff7dc59
SangHun Lee <ayteneve93@gmail.com>
2020-10-26 18:07:59 +0900

    First Commit

--- dev/GitPushingTask.js
+++ dev/GitPushingTask.js
@@ -7,7 +7,7 @@ module.exports.gitPush = async (defaultMessage = "No Commit Message", newVersion
     commitMessage = await commandExecutor.readPromisifiedText(`\nGit Commit Message (Default : ${defaultMessage}) : `)
     if(!commitMessage) commitMessage = defaultMessage
     await commandExecutor.runPromisifiedCommand(`git add -A`, false)
-    await commandExecutor.runPromisifiedCommand(`git commit =m "${commitMessage}"`)
+    await commandExecutor.runPromisifiedCommand(`git commit -m "${commitMessage}"`)
     if(newVersionTag) await commandExecutor.runPromisifiedCommand(`git tag "${newVersionTag}"`)
     await commandExecutor.runPromisifiedCommand(`git push origin master --tags`)
     
