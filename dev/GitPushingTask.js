4ee4a2b3608b31bc981a2da4cd39d9dc148b9861
SangHun Lee <ayteneve93@gmail.com>
2020-10-26 18:19:50 +0900

    No Commit Message

--- dev/GitPushingTask.js
+++ dev/GitPushingTask.js
@@ -10,7 +10,10 @@ module.exports.gitPush = async (defaultMessage = "No Commit Message", newVersion
     await commandExecutor.runPromisifiedCommand(`git commit -m "${commitMessage}"`)
     if(newVersionTag) await commandExecutor.runPromisifiedCommand(`git tag "${newVersionTag}"`)
     // await commandExecutor.runPromisifiedCommand(`git push origin master --tags`)
-    await commandExecutor.runPromisifiedCommand('git push')
-    
+    await commandExecutor.runPromisifiedCommand('git push --tags')
+    if(process.argv[2] == '-m') {
+        console.log("Git pushing completed")
+        process.exit(0)
+    }
 }
 if(process.argv[2] == '-m') module.exports.gitPush()
